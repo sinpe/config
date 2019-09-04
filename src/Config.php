00906421c5c2f261b78632fdae217c7ca61a3a58
sinpe <18222544@qq.com>
2019-09-04 16:06:31 +0800

    fix

--- src/Config.php
+++ src/Config.php
@@ -1,5 +1,5 @@
 <?php
- /*
+/*
  * This file is part of the long/config package.
  *
  * (c) Sinpe <support@sinpe.com>
@@ -9,8 +9,6 @@
  */
 
 namespace Sinpe\Config;
-
-use Illuminate\Support\Arr;
 
 /**
  * Configuration.
@@ -65,7 +63,7 @@ class Config implements ConfigInterface, \ArrayAccess
      *
      * @param  string  $key
      * @param  bool  $append
-     * @return self
+     * @return static
      */
     public function setPath(string $path, bool $append = false)
     {
@@ -84,7 +82,7 @@ class Config implements ConfigInterface, \ArrayAccess
      * Set the callback after file loaded.
      *
      * @param  callable  $callback
-     * @return self
+     * @return static
      */
     public function setCallback(callable $callback)
     {
@@ -97,7 +95,7 @@ class Config implements ConfigInterface, \ArrayAccess
      * Load config from file.
      *
      * @param  string  $file
-     * @return self
+     * @return static
      */
     public function load(string $file)
     {
@@ -152,7 +150,7 @@ class Config implements ConfigInterface, \ArrayAccess
     {
         $keys = is_array($key) ? $key : func_get_args();
 
-        return Arr::has($this->items, $keys);
+        return static::hasData($this->items, $keys);
     }
 
     /**
@@ -169,7 +167,7 @@ class Config implements ConfigInterface, \ArrayAccess
             return $this->getMany($key);
         }
 
-        $value = Arr::get($this->items, $key, $default);
+        $value = static::getData($this->items, $key, $default);
 
         if (is_null($value)) {
             // load
@@ -198,7 +196,7 @@ class Config implements ConfigInterface, \ArrayAccess
             if (is_numeric($key)) {
                 list($key, $default) = [$default, null];
             }
-            $config[$key] = $this->get($key, $default); 
+            $config[$key] = $this->get($key, $default);
         }
 
         return $config;
@@ -217,7 +215,7 @@ class Config implements ConfigInterface, \ArrayAccess
         $keys = is_array($key) ? $key : [$key => $value];
 
         foreach ($keys as $key => $value) {
-            Arr::set($this->items, $key, $value);
+            static::setData($this->items, $key, $value);
         }
     }
 
@@ -239,7 +237,7 @@ class Config implements ConfigInterface, \ArrayAccess
      */
     public function remove($keys)
     {
-        foreach ((array)$keys as $key) {
+        foreach ((array) $keys as $key) {
             $this->offsetUnset($key);
         }
         return $this;
@@ -288,5 +286,107 @@ class Config implements ConfigInterface, \ArrayAccess
     public function offsetUnset($key)
     {
         unset($this->items[$key]);
+    }
+
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array  $data
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    private static function getData(array $data, string $key = '', $default = null)
+    {
+        if (empty($key)) {
+            return $data;
+        }
+
+        if (array_key_exists($key, $data)) {
+            return $data[$key];
+        }
+
+        if (strpos($key, '.') === false) {
+            return $data[$key] ?? value($default);
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if (array_key_exists($segment, $data)) {
+                $data = $data[$segment];
+            } else {
+                return value($default);
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Check if an item or items exist in an array using "dot" notation.
+     *
+     * @param  array  $data
+     * @param  string|array  $keys
+     * @return bool
+     */
+    private static function hasData(array $data, $keys)
+    {
+        $keys = (array) $keys;
+
+        if (!$data || $keys === []) {
+            return false;
+        }
+
+        foreach ($keys as $key) {
+
+            if (array_key_exists($key, $data)) {
+                continue;
+            }
+
+            foreach (explode('.', $key) as $segment) {
+                if (array_key_exists($segment, $data)) {
+                    $data = $data[$segment];
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Set an array item to a given value using "dot" notation.
+     *
+     * If no key is given to the method, the entire array will be replaced.
+     *
+     * @param  array   $data
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return array
+     */
+    private static function setData(array &$data, string $key = '', $value = null)
+    {
+        if (empty($key)) {
+            return $data;
+        }
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1) {
+            // 
+            $key = array_shift($keys);
+            // If the key doesn't exist at this depth, we will just create an empty array
+            // to hold the next value, allowing us to create the arrays to hold final
+            // values at the correct depth. Then we'll keep digging into the array.
+            if (!isset($data[$key]) || !is_array($data[$key])) {
+                $data[$key] = [];
+            }
+
+            $data = &$data[$key];
+        }
+
+        $data[array_shift($keys)] = $value;
+
+        return $data;
     }
 }
